8bbe0109e13430079adef0257474ef7c197fd385
Paulius Saulėnas <psaulenas@kayak.com>
2021-04-21 16:36:30 +0300

    Validation

--- src/schema/resolvers/breedTranslation.ts
+++ src/schema/resolvers/breedTranslation.ts
@@ -1,5 +1,6 @@
 import { IResolvers } from 'graphql-tools';
 import { PubSub } from 'apollo-server-express';
+import { Validator } from 'node-input-validator';
 import createBreedTranslationQuery from '../../sql-queries/breedTranslation';
 
 const pubsub = new PubSub();
@@ -8,6 +9,18 @@ const BREED_CREATED = 'BREED_CREATED';
 const resolvers: IResolvers = {
     Mutation: {
         createBreedTranslation: async (_, { input }, { pgClient }) => {
+            const v = new Validator(input, {
+                breed: 'required',
+                language: 'required|maxLength:4',
+                translation: 'required',
+            });
+
+            const matched = await v.check();
+
+            if (!matched) {
+                throw new Error(JSON.stringify(v.errors));
+            }
+
             const dbResponse = await pgClient.query(
                 createBreedTranslationQuery(input)
             );
